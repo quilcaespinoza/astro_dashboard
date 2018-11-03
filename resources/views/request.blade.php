15f40ef9ddc2ebc0e432b5a8e29f118b0c00065f
Jose <quilca.espinoza@gmail.com>
2018-11-02 22:40:01 -0500

    email

--- resources/views/request.blade.php
+++ resources/views/request.blade.php
@@ -54,7 +54,7 @@
 
 
 @section("modal")
-    <div class="modal fade" id="modalFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade mdn" id="modalFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
